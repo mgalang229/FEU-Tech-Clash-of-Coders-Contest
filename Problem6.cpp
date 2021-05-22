ac73595615f6f0a5c2a3a423fe2443192ffd92df
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-22 18:27:46 +0800

    Create Problem6.cpp

---
+++ Problem6.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    string s;
+    cin >> n >> s;
+    sort(s.begin(), s.end());
+    do {
+        cout << s << '\n';
+    } while(next_permutation(s.begin(), s.end()));
+    return 0;
+}
